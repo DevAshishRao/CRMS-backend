76c51a836658331d15bb6da2d5dc60941c2ab0d5
DevAshishRao <devashishrao2003@gmail.com>
2025-11-28 10:17:48 +0530

    Models Created

---
+++ models/CrimeRecord.js
@@ -0,0 +1,44 @@
+// importing mongoose
+import mongoose from 'mongoose';
+
+const crimeSchema = new mongoose.Schema({ 
+
+    title: {
+        type: String,
+        required: true
+    },
+
+    description: {
+        type: String,
+        required: true,
+    },
+
+    status: {
+        type: String,
+        required: true    
+    },
+
+    crimeType: {
+        type: String,
+        required: true,
+    },
+
+    criminalName: {
+        type: String,
+        required: true
+    },
+
+    location: {
+        type: String,
+        required: true
+    },
+
+    createdAt: {
+        type: Date,
+    }
+
+})
+
+// exporting the schema
+const CrimeRecord = Schema('CrimeRecord',crimeSchema);
+export default CrimeRecord;
---
+++ models/User.js
@@ -0,0 +1,42 @@
+// importing mongoose
+import mongoose from 'mongoose';
+
+// defining user schema
+const userSchema = new mongoose.Schema({
+
+    username: {
+        type: String,
+        unique: true,
+        required: true
+    },
+
+    fullName: {
+        type: String,
+        required: true
+    },
+
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+
+    password: {
+        type: String,
+        unique: true
+    },
+
+    category: {
+        type: String
+    },
+
+    createdAt: {
+        type: Date,
+        requried: true,
+    }
+
+})
+
+// exporting the model
+const User = mongoose.model('User',userSchema);
+export default User;
