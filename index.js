c1465a6d0be43e08713fd1edc0378f4288fe3747
DevAshishRao <devashishrao2003@gmail.com>
2025-11-29 10:49:02 +0530

    Final Code Configuration - Trail 1

--- index.js
+++ index.js
@@ -30,8 +30,3 @@ app.use('/api/records',records);
 app.get("/", (req, res) => {
 res.send("API is running !");
 });
-
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-console.log(`Server is running on port : ${PORT}`);
-});
