f29bb34cb21514318783117f68ab1da02d65b460
DevAshishRao <devashishrao2003@gmail.com>
2025-11-29 10:06:13 +0530

    App listining Removed

--- index.js
+++ index.js
@@ -31,9 +31,4 @@ app.get("/", (req, res) => {
 res.send("API is running !");
 });
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-console.log(`Server is running on port : ${PORT}`);
-});
-
 export default app;
