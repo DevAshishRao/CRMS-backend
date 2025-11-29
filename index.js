e6e16368c8a0f829f503283acb5146574cb0479c
DevAshishRao <devashishrao2003@gmail.com>
2025-11-29 10:25:40 +0530

    Some configuration added

--- index.js
+++ index.js
@@ -31,4 +31,7 @@ app.get("/", (req, res) => {
 res.send("API is running !");
 });
 
-export default app;
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+console.log(`Server is running on port : ${PORT}`);
+});
